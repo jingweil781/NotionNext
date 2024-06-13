865ad52bc643a76a2c35db54f9b0ff5a2529c0da
jingweil781 <jingweil781@gmail.com>
2024-06-13 14:53:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'JingweiLi', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '欢迎关注，共同追逐新科技', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.woshipm.com/u/812972', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '数字化营销, AI', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'jingweil781@gmail.com', // 邮箱地址 例如mail@tangly1024.com
